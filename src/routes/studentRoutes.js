a001bc8833f5467b1789c7595494feb255d470fb
Jared0103 <josejared8ac@hotmail.com>
2024-05-19 22:55:20 -0600

    agrege verifytoken

--- src/routes/studentRoutes.js
+++ src/routes/studentRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const { addStudent, getAllStudents, updateStudent, deleteStudent } = require('../controllers/studentController');
 const { verifyToken } = require('../middleware/authMiddleware');
 
-router.post('/addStudent', addStudent);
-router.get('/getAllStudents', getAllStudents);
-router.put('/updateStudent/:id',  updateStudent);
-router.delete('/deleteStudent/:id',  deleteStudent);
+router.post('/addStudent', verifyToken, addStudent);
+router.get('/getAllStudents', verifyToken, getAllStudents);
+router.put('/updateStudent/:id', verifyToken,  updateStudent);
+router.delete('/deleteStudent/:id', verifyToken,  deleteStudent);
 
 module.exports = router;
--- src/routes/teacherRoutes.js
+++ src/routes/teacherRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const { addTeacher, getAllTeachers, updateTeacher, deleteTeacher } = require('../controllers/teacherController');
 const { verifyToken } = require('../middleware/authMiddleware');
 
-router.post('/addTeacher',  addTeacher);
-router.get('/getAllTeachers',  getAllTeachers);
-router.put('/updateTeacher/:id',  updateTeacher);
-router.delete('/deleteTeacher/:id',  deleteTeacher);
+router.post('/addTeacher', verifyToken,  addTeacher);
+router.get('/getAllTeachers', verifyToken, getAllTeachers);
+router.put('/updateTeacher/:id', verifyToken, updateTeacher);
+router.delete('/deleteTeacher/:id', verifyToken, deleteTeacher);
 
 module.exports = router;
